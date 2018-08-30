a0faf3021b32870b38345ba6225f72a38bcc4547
EmmanuelArethens <emmanuelarethens1@gmail.com>
2018-08-30 13:36:16 +0200

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,44 @@
+import * as types from "../constants/ActionTypes";
+
+const initialState = {
+    questionLoader: false,
+    questions: [],
+    choices: [],
+    showSlide: 0
+};
+
+const questionnaireReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        case types.ADD_QUESTIONS:
+            return {
+                ...state,
+                questions: action.questions
+            }
+        case types.UPDATE_QUESTION_LOADER:
+            return{
+                ...state,
+                questionLoader: action.display
+            }
+
+        case types.CHANGE_SLIDE:
+            return {
+            }
+        case types.UPDATE_CHOICE:
+            const filteredChoices = (state.choices.filter(choice => choice.id_question !== action.id_question));
+            return {
+                ...state,
+                choices: [
+                    ...filteredChoices,
+                    {
+                        id_question: action.id_question,
+                        id_answer: action.id_answer
+                    }
+                ]
+            }
+
+        default:
+            return state;
+    }
+};
+export default questionnaireReducer;
